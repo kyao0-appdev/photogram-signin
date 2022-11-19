e7b677d73a2933927e86e5fa143d330fcd598d35
kyao0 <kyao0@chicagobooth.edu>
2022-11-19 15:37:45 +0000

    0%

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
+  def register
+    render({ :template => "users/register.html.erb"})
+  end
+
+  def new_session_form
+    render({ :template => "users/signin_form.html.erb"})
+  end
+
+  def authenticate
+    un = params.fetch("input_username")
+    pw = params.fetch("input_username")
+
+    user = User.where({ :username => un }).at(0)
+
+    if user == nil
+      redirect_to("/user_sign_in", {alert => "No one by that name 'round these parts"})
+    else 
+      if user.authenticate(pw) 
+        session.store(:user_id,user.id)
+        redirect_to("/", { :notice => "Welcome back, " + user.username})
+      else
+        redirect_to("user_sign_in:", { :alert => "Nice try!"})
+    render({ :plain => "hi"})
+  end
+
+
+  def toast_cookies
+      reset_session
+
+      redirect_to("/", { :notice => "see you later!"})
+  end
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,10 +47,17 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_passsword_conf")
 
-    user.save
+    save_status = user.save
+    if save_status == true
+      session.store(:user_id, user.id)
 
-    redirect_to("/users/#{user.username}")
+      redirect_to("/users/<%= user.username%>", { :notice => "Welcome, " + user.username + "!"})
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors/full_messages.to_sentence})
+    end
   end
 
   def update
